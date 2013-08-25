028e869211a799135b3f3d1a884188a2b9b91b13
justin evaniew <justinevaniew@justin-computer.local>
2013-08-25 19:41:09 -0300

    GO!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DevLunch::Application.config.secret_key_base = '47abfc0a1359a4191e299adcf06653b6c1925a0d5906ae8ac461bad89dd8f294bba00486b9b83ee8cbfb637b6f4e12f07881af34b78c39e002148aa9046f38b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DevLunch::Application.config.session_store :cookie_store, key: '_devLunch_session'
